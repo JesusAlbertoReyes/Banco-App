fef1c11935aca4e54826f170493f95b1aa186aba
JesusAlbertoReyes <tachiloko422@hotmail.com>
2023-12-10 14:37:36 -0700

    Clase Cuenta Bancaria

---
+++ app/src/main/java/com/example/bancoapp/Cuenta.java
@@ -0,0 +1,55 @@
+package com.example.cuentadebanco;
+
+import java.io.Serializable;
+
+public class Cuenta implements Serializable {
+
+    String numCuenta;
+    String nombre;
+    String banco;
+    float saldo;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(String numCuenta, String nombre, String banco, float saldo) {
+        this.numCuenta = numCuenta;
+        this.nombre = nombre;
+        this.banco = banco;
+        this.saldo = saldo;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+}
+
